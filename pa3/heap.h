040f89419452ebe3fdc5a9bed00dcc62ebebe5ac
Thanh Ngo <felix.infinite@gmail.com>
2015-10-14 19:04:34 -0400

    Add pa3

---
+++ pa3/heap.h
@@ -0,0 +1,11 @@
+#ifndef HEAP_H
+#define HEAP_H
+#endif
+/*
+ *  My malloc implementation
+ */
+void* customMalloc(int , char*, int);
+/*
+ *  My free implementation
+ */ 
+void customFree(void*, char*, int);
---
+++ pa3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "heap.h" 
+
+#define malloc(x) customMalloc(x,__FILE__,__LINE__);
+#define free(x) customFree(x,__FILE__,__LINE__);
+
+
+int main(int argc, char* argv[]){
+    
+    int x =100;
+    free(&x);
+    
+    char* hoo = (char*) malloc(10);
+    free(hoo);
+    free(hoo);
+
+    char* charP =(char*) malloc(100);
+    free(charP);
+    
+    charP = (char*)malloc(10);
+    free(charP);
+    
+    return 0;
+}
