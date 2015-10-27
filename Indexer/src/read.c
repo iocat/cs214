be7352e703a2cbf184609aa9e2d569c62d99deba
Thanh Ngo <felix.infinite@gmail.com>
2015-10-27 13:33:44 -0400

    Add read.c and read.h
    read.c and read.h allow uer to read the file

---
+++ Indexer/src/read.c
@@ -0,0 +1,31 @@
+#include "read.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char* read_file(char* path){
+    int fd = open(path,O_RDONLY);
+    int length=fd_len(fd);
+    char* bf;
+    if(fd == -1){
+        fprintf(stderr,"Cannot open file %s",path);
+        exit(EXIT_FAILURE);
+    }else{
+        lseek(fd,0,SEEK_SET);
+        bf = malloc( length+1);
+        read(fd,bf,length);
+        bf[length] = '\0';      
+    }   
+    return bf;
+}
+
+int fd_len(int fd){
+    int current_pos = lseek(fd,0,SEEK_CUR);
+    int length = lseek(fd,0,SEEK_END);
+    lseek(fd,cur_pos,SEEK_SET);
+    return length;
+}
+
